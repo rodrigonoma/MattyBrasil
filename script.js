1d97f1d6e885343c36f3135808fa75782e286c1e
rodrigonoma <rodrigonoma@gmail.com>
2020-06-23 20:16:23 -0300

    Adicionados os arquivos v1.0

---
+++ script.js
@@ -0,0 +1,79 @@
+// Identificar o clique no menu
+// Verificar o item que foi clicado e fazer referência com o alvo
+// Verificar a distância entre o alvo e o topo
+// Animar o scroll até o alvo
+
+const menuItems = document.querySelectorAll('.menu a[href^="#"]');
+
+function getScrollTopByHref(element) {
+	const id = element.getAttribute('href');
+	return document.querySelector(id).offsetTop;
+}
+
+function scrollToPosition(to) {
+  // Caso queira o nativo apenas
+	// window.scroll({
+	// top: to,
+	// behavior: "smooth",
+	// })
+  smoothScrollTo(0, to);
+}
+
+function scrollToIdOnClick(event) {
+	event.preventDefault();
+	const to = getScrollTopByHref(event.currentTarget)- 80;
+	scrollToPosition(to);
+}
+
+menuItems.forEach(item => {
+	item.addEventListener('click', scrollToIdOnClick);
+});
+
+// Caso deseje suporte a browsers antigos / que não suportam scroll smooth nativo
+/**
+ * Smooth scroll animation
+ * @param {int} endX: destination x coordinate
+ * @param {int) endY: destination y coordinate
+ * @param {int} duration: animation duration in ms
+ */
+function smoothScrollTo(endX, endY, duration) {
+  const startX = window.scrollX || window.pageXOffset;
+  const startY = window.scrollY || window.pageYOffset;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
+
+  duration = typeof duration !== 'undefined' ? duration : 400;
+
+  // Easing function
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+  }, 1000 / 60); // 60 fps
+};
+
+// var banner = document.querySelector('.imgTopo');
+
+// const imagens = [
+//     'img/topo.jpg',
+//     'img/banner2.png',
+//     'img/banner3.png'
+// ];
+// imagens.forEach(img=>{
+//     const divImg = document.createElement('div');
+//     divImg.style.backgroundImage = `url('${img}')`;
+//     divImg.classList.add('imgTopoChilds');
+//     banner.appendChild(divImg);
+// })
+
+// banner.style.transition = 'all .8s';
